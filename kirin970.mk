751e7fcdc90df26cc62995305f5e970ae4d143f8
hak86 <hakamelassouad@gmail.com>
2019-10-13 18:24:19 +0200

    kirin970-common: Copy handheld core hardware
    * Fixes some issues with Gapps

--- kirin970.mk
+++ kirin970.mk
@@ -63,7 +63,8 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Radio
 PRODUCT_PACKAGES += \
